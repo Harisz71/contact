ba30fe13f60188386898724a52814646d1ff6633
Haris Zubair <hariszubair71@gmail.com>
2024-07-28 05:14:06 +0500

    registration

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,15 +1,33 @@
-      document.querySelector('#mainloginpage').addEventListener('click', showloginbox);
-      document.querySelector('#loginDialogue #cancelbtn').addEventListener('click', (e)=>
-    { 
-      removeloginbox();
-    });
-        function showloginbox()
-    {
-        let loginDialogue= document.querySelector('#loginDialogue');
-        loginDialogue.classList.add('active');
-    }
-    function removeloginbox()
-    {
-      let loginDialogue= document.querySelector('#loginDialogue');
-      loginDialogue.classList.remove('active');
-    }
+document
+  .querySelector("#mainloginpage")
+  .addEventListener("click", showloginbox);
+document
+  .querySelector("#loginDialogue .cancelbtn")
+  .addEventListener("click", (e) => {
+    removeloginbox();
+  });
+function showloginbox() {
+  let loginDialogue = document.querySelector("#loginDialogue");
+  loginDialogue.classList.add("active");
+}
+function removeloginbox() {
+  let loginDialogue = document.querySelector("#loginDialogue");
+  loginDialogue.classList.remove("active");
+}
+
+//register Dialogue
+
+document
+  .querySelector("#mainregisterbtn")
+  .addEventListener("click", showregisterbox);
+document
+  .querySelector("#registerDialogue .cancelbtn")
+  .addEventListener("click", hideregisterbox);
+function showregisterbox() {
+  let registerDialogue = document.querySelector("#registerDialogue");
+  registerDialogue.classList.add("active");
+}
+function hideregisterbox() {
+  let registerDialogue = document.querySelector("#registerDialogue");
+  registerDialogue.classList.remove("active");
+}
