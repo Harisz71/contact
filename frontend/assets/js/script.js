8e4c2230cb2482e687aa1e59b24485f0aa8276b0
Haris Zubair <hariszubair71@gmail.com>
2024-07-28 21:47:09 +0500

    login validation

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -31,3 +31,16 @@ function hideregisterbox() {
   let registerDialogue = document.querySelector("#registerDialogue");
   registerDialogue.classList.remove("active");
 }
+function formchange() {
+  let form = document.querySelector("#loginDialogue form");
+  let email = form.querySelector('input[type="email"]').value;
+  let password = form.querySelector('input[type="password"]').value;
+
+  if (email.length > 0 && password.length > 0) {
+    form.querySelector('button[type="submit"]').removeAttribute("disabled");
+  } else {
+    form
+    .querySelector('button[type="submit"]')
+    .setAttribute("disabled", "disabled");
+  }
+}
