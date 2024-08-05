b7d6c17ef3d8a01925991efe3e0071abb5a40b89
Haris Zubair <hariszubair71@gmail.com>
2024-08-06 01:45:29 +0500

    header> serach suggestion ui completed

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,27 +2,33 @@ function onSearchchange(e) {
   let searchbox = document.querySelector(
     "#searchbox #inputcontainer .cross"
   ).classList;
-  if (e.target.value.length > 0) searchbox.add("active");
-  else {
+  let suggestions = document.querySelector(".suggestions").classList;
+
+  if (e.target.value.length > 0) {
+    searchbox.add("active");
+    suggestions.add("active");
+  } else {
     searchbox.remove("active");
+    suggestions.remove("active");
   }
 }
 function removeboxcontent() {
   let input = document.querySelector("#searchbox input");
+  let suggestions = document.querySelector(".suggestions").classList;
   input.value = "";
   document
     .querySelector("#searchbox #inputcontainer .cross")
     .classList.remove("active");
+    suggestions.remove("active");
 }
-function injectheader()
-{
-  fetch('../component/dashboard/header.component.html').then((r)=>
-  {
-    return r.text();
-  }).then((container)=>
-  {
-    document.querySelector('#head').innerHTML= container; 
-  });
+function injectheader() {
+  fetch("../component/dashboard/header.component.html")
+    .then((r) => {
+      return r.text();
+    })
+    .then((container) => {
+      document.querySelector("#head").innerHTML = container;
+    });
 }
 injectheader();
 function injectsidebar() {
